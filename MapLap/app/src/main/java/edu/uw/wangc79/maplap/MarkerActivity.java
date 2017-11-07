10986b9417136707c82c027f861fb406204da971
WANG CHEN <wangc79@uw.edu>
2017-11-06 19:32:37 -0800

    Complete lab-maps

---
+++ MapLap/app/src/main/java/edu/uw/wangc79/maplap/MarkerActivity.java
@@ -0,0 +1,10 @@
+package edu.uw.wangc79.maplap;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wangchen on 11/6/17.
+ */
+
+public class MarkerActivity extends AppCompatActivity {
+}
